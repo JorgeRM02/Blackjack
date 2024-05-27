0345a2823f60c93cddcc47ff8bbfe2ae9ec7ce80
ÁREA DESARROLLO 1 <REA DESARROLLO 1@DESARROLLO14.local>
2024-05-27 11:36:22 +0200

    Modificado el constructor y añadido el metodo valorMano que devuelve la puntuacion de la mano.

--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -1,8 +1,18 @@
 package modelo;
 
-public class Mano extends Mazo {
+import java.util.ArrayList;
 
+public class Mano extends Mazo {
+	
 	public Mano() {
+		this.cartas = new ArrayList<Carta>();
 	}
 	
+	public int valorMano() {
+		int valor=0;
+		for(Carta c : this.cartas) {
+			valor =  valor + c.GetValor();
+		}
+		return valor;
+	}
 }
