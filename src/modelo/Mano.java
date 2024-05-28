35a18f00a85f39439f1f064bd7b1eadb7896d5c3
ÁREA DESARROLLO 1 <REA DESARROLLO 1@DESARROLLO14.local>
2024-05-28 09:15:32 +0200

    Modificado y finalizado el método valorMano(), implementado el método finDeJuego() e implementado el método toString().

--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -10,9 +10,39 @@ public class Mano extends Mazo {
 	
 	public int valorMano() {
 		int valor=0;
+		int numAses=0;
 		for(Carta c : this.cartas) {
+			if (c.getNumero()==1) {
+				numAses++;
+			}
 			valor =  valor + c.GetValor();
+		}
+		// A continuacion si el numero pasa de 21 y hay ases en la mano
+		if (numAses>0 && valor>21) {
+			valor=valor-10;
+			numAses--;
 		}
 		return valor;
 	}
+	
+	// El siguiente método se comprueba si hay mas de 21 puntos en la mano e indica el fin del juego.
+	public boolean finDeJuego() {
+		if(valorMano()>=21) {
+			return true;
+		}
+			return false;	
+		}
+
+	@Override
+	public String toString() {
+		return "Mano [cartas=" + cartas + "] Valor de la mano: " + valorMano();
+	}
+	
+	
+	
 }
+
+		
+	
+	
+
